700bdd2f6751d2afc8aee01604cce6faf174231e
Jason Greenwood <2datop@gmail.com>
2012-03-18 02:18:35 +1300

    Initial commit of Template for research site of Eye-Gaze and web performance analysis

---
+++ WebAppPerformanceAnalysis/Controllers/EyegazeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPerformanceAnalysis.Controllers
+{
+    public class EyegazeController : Controller
+    {
+        //
+        // GET: /EyeGaze/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            return View();
+        }
+
+        public ActionResult TechSearchArticle()
+        {
+            return View();
+        }
+
+        public ActionResult StandardSearchArticle()
+        {
+            return View();
+        }
+
+        public ActionResult TechReadArticle()
+        {
+            return View();
+        }
+
+        public ActionResult StandardReadArticle()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppPerformanceAnalysis/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppPerformanceAnalysis.MvcApplication" Language="C#" %>
