e5be245968fb8969d03320c4552271dcee146bbe
Bridget Beh <bridget.beh@gmail.com>
2012-03-26 11:24:55 +1300

    Added a link to open a media page without threading.

--- WebAppPerformanceAnalysis/Controllers/HomeController.cs
+++ WebAppPerformanceAnalysis/Controllers/HomeController.cs
@@ -19,5 +19,7 @@ namespace WebAppPerformanceAnalysis.Controllers
         {
             return View();
         }
+
+
     }
 }
---
+++ WebAppPerformanceAnalysis/Controllers/MediaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Timers;
+
+namespace WebAppPerformanceAnalysis.Controllers
+{
+    public class MediaController : Controller
+    {
+        //
+        // GET: /Media/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Media Tests";
+            return View();
+        }
+
+        public ActionResult LoadUnicorns()
+        {
+            return View("Unicorns");
+        }
+    }
+}
