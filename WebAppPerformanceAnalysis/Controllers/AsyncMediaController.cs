87b23c9f994dc806265596701f66c17b9a7e5fa1
Bridget Beh <bridget.beh@gmail.com>
2012-04-01 01:32:22 +1300

    Tidied up media page - now loads 40 or so images using asynchronous method calls

--- WebAppPerformanceAnalysis/Controllers/AsyncMediaController.cs
+++ WebAppPerformanceAnalysis/Controllers/AsyncMediaController.cs
@@ -6,13 +6,14 @@ using System.Web.Mvc;
 using System.Timers;
 using System.Threading;
 using System.Threading.Tasks;
+using System.ComponentModel;
 using System.IO;
 
 namespace WebAppPerformanceAnalysis.Controllers
 {
     public class AsyncMediaController : AsyncController
     {
-        //
+        
         // GET: /Media/
 
         public ActionResult Index()
@@ -28,56 +29,117 @@ namespace WebAppPerformanceAnalysis.Controllers
 
         public void UnicornsAsync()
         {
-            string pwd = @"C:\Users\BB\WebAppPerformanceAnalysis\WebAppPerformanceAnalysis\Controllers"; // Need to get the path to the "Content" folder??
-            pwd = pwd.Remove(pwd.LastIndexOf('\\')) + "\\Content";
-            Console.WriteLine(pwd);
-            string[] fileEntries = Directory.GetFiles(pwd);
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
+            DirectoryInfo d = Directory.GetParent(Directory.GetParent(a.CodeBase.ToString().Substring(8)).ToString());
+            
+            string[] fileEntries = Directory.GetFiles(d.ToString()+@"/Content", "*.jpg");
             var files = from f in fileEntries 
                         where f.EndsWith(".jpg") || f.EndsWith(".png")
                         select f;
             AsyncManager.Timeout = 10000;
             
-            for (int j = 0; j < 1; j++)
+            for (int j = 0; j < 5; j++)
             {
                 foreach (var fileName in files)
                 {
                     
                     AsyncManager.OutstandingOperations.Increment();
-                    var task = Task.Factory.StartNew(() => loadImage((string)fileName));
-                    
+                    //var task = Task.Factory.StartNew(() => loadImage((string)fileName));
+                    //Task t = new Task(() => loadImage((string)fileName));
+                    //t.Start();
+                    MediaLoader l = new MediaLoader();
+                    l.LoadImageCompleted += (sender, e) =>
+                    {
+                        Response.Write("<img alt=\"\" src=\"data:image/jpeg;base64," + @e.ImageContents + "\" />");
+                        Response.Flush();
+                        AsyncManager.OutstandingOperations.Decrement();
+                    };
+
+                    l.LoadImageAsync((string)fileName);
                 }
             }
+            
             UnicornsCompleted();
         }
 
-        private void loadImage(string file)
+        
+    }
+
+    public delegate void LoadImageCompletedEventHandler(object sender, LoadImageCompletedEventArgs e);
+
+        
+    public class MediaLoader : Component
+    {
+        public event LoadImageCompletedEventHandler LoadImageCompleted;
+        
+        private SendOrPostCallback onCompletedDelegate;
+
+        public MediaLoader()
+        {
+            InitializeDelegates();
+        }
+
+        protected virtual void InitializeDelegates()
+        {
+            onCompletedDelegate = new SendOrPostCallback(LoadCompleted);
+        }
+
+        public string LoadImageAsync(string file)
         {
             // Render image...
             FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
             byte[] buffer = new byte[fileStream.Length];
             fileStream.Read(buffer, 0, (int)fileStream.Length);
             fileStream.Close();
-            //Response.WriteFile(file);
-            //Response.ContentType = "image/jpg";
             string base64string = Convert.ToBase64String(buffer);
-            
-            Response.Write("<img alt=\"\" src=\"data:image/jpeg;base64,"+@base64string+"\" />");
-            Response.Flush();
-            AsyncManager.OutstandingOperations.Decrement();
+            LoadCompleted(new LoadImageCompletedEventArgs(base64string, null, false, null));
+            return base64string;
+        }
+
+        private void LoadCompleted(object operationState)
+        {
+            LoadImageCompletedEventArgs e = operationState as LoadImageCompletedEventArgs;
+
+            OnLoadImageCompleted(e);
+        }
+
+        protected void OnLoadImageCompleted(LoadImageCompletedEventArgs e)
+        {
+            if (LoadImageCompleted != null)
+            {
+                LoadImageCompleted(this, e);
+            }
+        }
+
+        private void CompletionMethod(string file, Exception exception, bool canceled, AsyncOperation asyncOp)
+        {
+            LoadImageCompletedEventArgs e = new LoadImageCompletedEventArgs(file, exception, canceled, asyncOp);
+
+            asyncOp.PostOperationCompleted(onCompletedDelegate, e);
+        }
+
+    }
+
+    public class LoadImageCompletedEventArgs : System.ComponentModel.AsyncCompletedEventArgs
+    {
+        private string imageContents;
+
+        public LoadImageCompletedEventArgs(string imageContents, Exception e, bool canceled, object state)
+            : base(e, canceled, state)
+        {
+            this.imageContents = imageContents;
+        }
+
+        public string ImageContents
+        {
+            get
+            {
+                RaiseExceptionIfNecessary();
+
+                return imageContents;
+            }
         }
     }
 
-    public class MediaLoader
-    {
-        private string file;
-        public MediaLoader(string filepath)
-        {
-            file = filepath;
-        }
-        
-        public void LoadAsync()
-        {
-            // Load image file
-        }
-    }
+
 }
