af622030fd96823450431ddb19e00980d6736b28
Jason Greenwood <2datop@gmail.com>
2012-05-10 11:45:02 +1200

    Bridgets Calculation view added

---
+++ WebAppPerformanceAnalysis/Controllers/ComputationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPerformanceAnalysis.Controllers
+{
+    public class ComputationController : Controller
+    {
+        //
+        // GET: /Computation/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "High Computation Tests";
+            return View();
+        }
+
+    }
+}
